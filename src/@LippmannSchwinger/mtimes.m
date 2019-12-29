8ee9e8735c6e87794b596dcadf078115075451fa
leonardo <leonardo@wave-laptop.local>
2019-12-29 12:51:32 -0800

    added a few asserts

--- src/@LippmannSchwinger/mtimes.m
+++ src/@LippmannSchwinger/mtimes.m
@@ -1,27 +1,17 @@
 function LSx = mtimes(LS,x)
-% MTIMES  Overload multiplication operator for Ham class
-%    HX = H*X returns a wavefun corresponding to H*X.
+% MTIMES  Overload multiplication operator for LippmannSchwinger class
+%    LSx = LS*x returns a vector 
 %
-%    See also Ham, Wavefun.
+%    See also LippmannSchwinger
 
 %  Copyright (c) 2019-2020 Leonardo Zepeda-Núñez
 
 % we need to assert that x is a vector and that is has the correct size
 assert(size(x,1) == LS.n*LS.m )
+% only works for vectors so far
+assert(size(x,2) == 1 )
 
-% BExt = zeros(LS.ne,LS.me);
-% BExt(1:LS.n,1:LS.m)= reshape(x,LS.n,LS.m);
-% 
-% % Fourier Transform
-% BFft = fftshift(fft2(BExt));
-% % Component-wise multiplication
-% BFft = LS.GFFT.*BFft;
-% % Inverse Fourier Transform
-% BExt = ifft2(ifftshift(BFft));
-% 
-% % multiplication by omega^2
-% B = BExt(1:LS.n, 1:LS.m);
-
+% by construction this will give back a matrix
 B = apply_Green(LS, x);
 
 LSx = -x + LS.omega^2*LS.nu.*B(:);
--- src/@LippmannSchwingerSlow/mtimes.m
+++ src/@LippmannSchwingerSlow/mtimes.m
@@ -1,13 +1,15 @@
 function LSmu = mtimes(LS,mu)
-% MTIMES  Overload multiplication operator for Ham class
-%    HX = H*X returns a wavefun corresponding to H*X.
+% MTIMES  Overload multiplication operator for LippmannSchwingerSlow class
+%    LSmu = LS*mu returns a vector 
 %
-%    See also Ham, Wavefun.
+%    See also LippmannSchwinger
 
 %  Copyright (c) 2019-2020 Leonardo Zepeda-Núñez
 
 % we need to assert that x is a vector and that is has the correct size
 assert(size(mu,1) == LS.n*LS.m )
+% it only works for vectors so far
+assert(size(mu,2) == 1 )
 
 u_inc = reshape(exp(1i*LS.omega*(LS.e(1)*LS.X+ LS.e(2)*LS.Y)), LS.n*LS.m, 1) ;
 b = mu.*u_inc;
